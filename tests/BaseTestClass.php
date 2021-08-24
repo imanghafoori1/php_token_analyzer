5926cb6b5da1cbb0a9fd317e2d8cabc0dc7a72bf
ali khosravi <alikhosravi175@gmail.com>
2021-08-24 23:21:29 +0430

    Transferred tests from laravel-microscope

---
+++ tests/BaseTestClass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Imanghafoori\TokenAnalyzer\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTestClass extends TestCase
+{
+    /**
+     * get tokens of stubs classes.
+     *
+     * @param string $path path of stub file
+     *
+     * @return array
+     */
+    protected function getTokens(string $path): array
+    {
+        return token_get_all(file_get_contents(__DIR__.$path));
+    }
+}
