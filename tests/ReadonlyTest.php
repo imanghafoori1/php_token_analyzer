0b8cbf9b257369bb1739b2d2685c9a20c1b400da
Iman Ghafoori <imanghafoori1@gmail.com>
2022-06-09 18:19:31 +0430

    add tests

--- tests/ReadonlyTest.php
+++ tests/ReadonlyTest.php
@@ -25,11 +25,16 @@ class ReadonlyTest extends BaseTestClass
         $tokens = token_get_all($string);
         [$output, $namespace] = ClassReferenceFinder::process($tokens);
 
+        $this->assertEquals('', $namespace);
         $this->assertEquals('tH0', $output[0][0][1]);
         $this->assertEquals('tH1', $output[1][0][1]);
         $this->assertEquals('t\H2', $output[2][0][1]);
         $this->assertEquals('\tH3', $output[3][0][1]);
         $this->assertEquals('tH4', $output[4][0][1]);
+        $this->assertEquals('tH5', $output[5][0][1]);
+        $this->assertEquals('tH6', $output[6][0][1]);
+        $this->assertEquals('tH7', $output[7][0][1]);
+        $this->assertEquals('tH8', $output[8][0][1]);
     }
 }
 
