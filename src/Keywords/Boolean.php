57bd1d0079d52b85d9f7c23d424381f943ca2ca6
Iman Ghafoori <imanghafoori1@gmail.com>
2023-02-06 15:27:53 +0330

    Fix issue

--- src/Keywords/Boolean.php
+++ src/Keywords/Boolean.php
@@ -11,8 +11,8 @@ class Boolean
         return in_array($token, [T_BOOLEAN_AND, T_BOOLEAN_OR, T_LOGICAL_OR, T_LOGICAL_AND], true);
     }
 
-    public static function body(ClassRefProperties $properties, &$tokens, &$t)
+    public static function body(ClassRefProperties $properties, &$tokens, &$token)
     {
-        return Semicolon::body($tokens, $t, $properties);
+        return Semicolon::body($properties, $tokens, $token);
     }
 }
